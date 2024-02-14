0c84de531a7a782b6638c3fcc45331942ff4ae8b
EC2 Default User <ec2-user@ip-172-31-33-89.ap-northeast-1.compute.internal>
2024-02-14 05:59:35 +0000

    PHP/Laravel 13 #6

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,6 +11,31 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>プロフィール新規作成</h2>
+                <form action="{{ route('admin.profile.create') }}" method="post" enctype="multipart/form-data"></form>
+                <div class="form-group row">
+                    <label class="col-md-2">氏名</label>
+                    <div class="col-md-10">
+                        <textarea class="form-control" name="name" rows="1">{{ old('name') }}</textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2">性別</label>
+                    <div class="col-md-10">
+                        <textarea class="form-control" name="gender" rows="1">{{ old('gender') }}</textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2">趣味</label>
+                    <div class="col-md-10">
+                        <textarea class="form-control" name="hobby" rows="20">{{ old('hobby') }}</textarea>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2">自己紹介</label>
+                    <div class="col-md-10">
+                        <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
